91342aff4d9576f8cb398f95e9f9db421b69c859
HeadlessHamster <joost_rietdijk@live.nl>
2022-07-05 18:51:29 +0200

    Check for update at startup

--- electron/barsysteem/src/index.js
+++ electron/barsysteem/src/index.js
@@ -16,10 +16,11 @@ ipcMain.on("klaarErmee", () =>{
 })
 
 let vkb;
+var mainWindow
 
 const createWindow = () => {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 1024,
     height: 600,
     webPreferences: {
@@ -48,56 +49,9 @@ const createWindow = () => {
 
     mainWindow.show()
     //mainWindow.webContents.openDevTools()
-
+    checkUpdate()
     ipcMain.on("checkUpdate", () => {
-      console.log("Checking for update")
-      let request = new XMLHttpRequest();
-    
-      var updateAvailable = false
-      var updateUrl;
-      request.onreadystatechange = function() {
-          if(this.readyState == 4 && this.status == 200){
-              let response = JSON.parse(this.responseText)
-    
-              //Get latest version of the app from the GitHub API
-              latestVersion = response["tag_name"].toString().replace('V', '');
-              //Convert the version string to a list for easier comparing
-              latestVersionList = latestVersion.split('.');
-    
-              //Get current app version and converting to list
-              currentVersion = app.getVersion().toString();
-              currentVersionList = currentVersion.split('.')
-    
-              console.log(latestVersionList);
-              console.log(currentVersionList);
-    
-              //Check if a new version is available
-              if(parseInt(latestVersionList[0]) > parseInt(currentVersionList[0])){
-                updateAvailable = true;
-              }else if(parseInt(latestVersionList[1]) > parseInt(currentVersionList[1]) && parseInt(latestVersionList[0]) >= parseInt(currentVersionList[0])){
-                updateAvailable = true;
-              }else if(parseInt(latestVersionList[2]) > parseInt(currentVersionList[2]) && parseInt(latestVersionList[1]) >= parseInt(currentVersionList[1]) && parseInt(latestVersionList[0]) >= parseInt(currentVersionList[0])){
-                updateAvailable = true;
-              }
-    
-              if(updateAvailable){
-                  console.log("New update!")
-                  for(asset in response["assets"]){
-                      if(response["assets"][asset]["name"].includes('AppImage')){
-                          updateUrl = response["assets"][asset]["browser_download_url"]
-                          break
-                      }
-                  }
-                  console.log(updateUrl)
-                  mainWindow.webContents.send("updateAvailable", updateUrl)
-              }else{
-                  console.log("No new update")
-              }
-          }
-      }
-    
-      request.open("GET", "https://api.github.com/repos/HeadlessHamsterr/barsysteem2/releases/latest")
-      request.send()
+      checkUpdate()
     })
   })
 };
@@ -123,6 +77,57 @@ app.on('activate', () => {
     createWindow();
   }
 });
+
+function checkUpdate(){
+  console.log("Checking for update")
+  let request = new XMLHttpRequest();
+
+  var updateAvailable = false
+  var updateUrl;
+  request.onreadystatechange = function() {
+      if(this.readyState == 4 && this.status == 200){
+          let response = JSON.parse(this.responseText)
+
+          //Get latest version of the app from the GitHub API
+          latestVersion = response["tag_name"].toString().replace('V', '');
+          //Convert the version string to a list for easier comparing
+          latestVersionList = latestVersion.split('.');
+
+          //Get current app version and converting to list
+          currentVersion = app.getVersion().toString();
+          currentVersionList = currentVersion.split('.')
+
+          console.log(latestVersionList);
+          console.log(currentVersionList);
+
+          //Check if a new version is available
+          if(parseInt(latestVersionList[0]) > parseInt(currentVersionList[0])){
+            updateAvailable = true;
+          }else if(parseInt(latestVersionList[1]) > parseInt(currentVersionList[1]) && parseInt(latestVersionList[0]) >= parseInt(currentVersionList[0])){
+            updateAvailable = true;
+          }else if(parseInt(latestVersionList[2]) > parseInt(currentVersionList[2]) && parseInt(latestVersionList[1]) >= parseInt(currentVersionList[1]) && parseInt(latestVersionList[0]) >= parseInt(currentVersionList[0])){
+            updateAvailable = true;
+          }
+
+          if(updateAvailable){
+              console.log("New update!")
+              for(asset in response["assets"]){
+                  if(response["assets"][asset]["name"].includes('AppImage')){
+                      updateUrl = response["assets"][asset]["browser_download_url"]
+                      break
+                  }
+              }
+              console.log(updateUrl)
+              mainWindow.webContents.send("updateAvailable", updateUrl)
+          }else{
+              console.log("No new update")
+          }
+      }
+  }
+
+  request.open("GET", "https://api.github.com/repos/HeadlessHamsterr/barsysteem2/releases/latest")
+  request.send()
+}
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
